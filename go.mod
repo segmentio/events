fb6050a8804ecaa46fe4cf38f4e42ad1fcf8e72d
Kevin Burke <kevin.burke@segment.com>
2023-11-30 12:49:11 -0800

    terminal: replace with golang.org/x/term
    At the time in 2017 that this code was added, this replaced an import
    in golang.org/x/crypto/ssh, which was completely reasonable, but now
    this behavior is available in golang.org/x/term which has a lot fewer
    things going on, so should just use that.

--- go.mod
+++ go.mod
@@ -5,7 +5,10 @@ go 1.20
 require (
 	github.com/pkg/errors v0.9.1
 	github.com/segmentio/encoding v0.3.6
-	golang.org/x/sys v0.6.0
+	golang.org/x/term v0.15.0
 )
 
-require github.com/segmentio/asm v1.1.3 // indirect
+require (
+	github.com/segmentio/asm v1.1.3 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+)
--- terminal.go
+++ terminal.go
@@ -1,20 +1,10 @@
-// Copyright 2011 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-//go:build darwin || dragonfly || freebsd || (linux && !appengine) || netbsd || openbsd
-// +build darwin dragonfly freebsd linux,!appengine netbsd openbsd
-
 package events
 
-import (
-	"syscall"
-	"unsafe"
-)
+import "golang.org/x/term"
 
 // IsTerminal returns true if the given file descriptor is a terminal.
+//
+// Deprecated: Use golang.org/x/term.IsTerminal instead.
 func IsTerminal(fd int) bool {
-	var termios syscall.Termios
-	_, _, err := syscall.Syscall6(syscall.SYS_IOCTL, uintptr(fd), ioctlReadTermios, uintptr(unsafe.Pointer(&termios)), 0, 0, 0)
-	return err == 0
+	return term.IsTerminal(fd)
 }
