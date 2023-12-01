f3bcd3d1255fc8900f9767c90ccd4e137e7754a1
Kevin Burke <kevin.burke@segment.com>
2023-11-30 22:53:25 -0800

    ecslogs,text: use golang.org/x/term

--- ecslogs/init.go
+++ ecslogs/init.go
@@ -8,10 +8,11 @@ import (
 	"path/filepath"
 
 	"github.com/segmentio/events/v2"
+	"golang.org/x/term"
 )
 
 func init() {
-	if !events.IsTerminal(1) {
+	if !term.IsTerminal(1) {
 		events.DefaultHandler = &Handler{
 			Output:  os.Stdout,
 			Program: filepath.Base(os.Args[0]),
